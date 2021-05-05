70d033f89aa95a28ddd3c09c029367f8186da050
ianbelfatto <ibelfatto@gmail.com>
2021-05-05 15:29:40 -0400

    adds rails seed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,5 @@
 class ProductsController < ApplicationController
+  def keyboard
+    render json: {message: "sanity check"}
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = Product.create(name: "Keyboard", price: 90, image_url: "https://assets3.razerzone.com/iJfeZ-X8GnTFBhH1nltN-tDuMt0=/1500x1000/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fh9c%2Fh39%2F9084456599582%2FRZ03-02640200-R3U1-5.jpg", description: "A tenkeyless keyboard" )
+product = Product.create(name: "Mouse", price: 100, image_url: "https://assets3.razerzone.com/iJfeZ-X8GnTFBhH1nltN-tDuMt0=/1500x1000/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fh9c%2Fh39%2F9084456599582%2FRZ03-02640200-R3U1-5.jpg", description: "A tenkeyless keyboard" )
+product = Product.create(name: "Mousepad", price: 10, image_url: "https://assets3.razerzone.com/iJfeZ-X8GnTFBhH1nltN-tDuMt0=/1500x1000/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fh9c%2Fh39%2F9084456599582%2FRZ03-02640200-R3U1-5.jpg", description: "A tenkeyless keyboard" )
