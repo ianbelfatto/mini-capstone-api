f04c7cc7520d55be1182bd0f08a8a2a89431eef0
ianbelfatto <ibelfatto@gmail.com>
2021-05-06 12:33:14 -0400

    changes terminal app to display products with titles and adds spacing return in terminal

--- products_terminal_app.rb
+++ products_terminal_app.rb
@@ -5,5 +5,14 @@ system "clear"
 puts "Here are all of the products available on the webpage!"
 puts ""
 
-response = HTTP.get("http://localhost:3000/all_products")
-puts response.parse
+mouse = HTTP.get("http://localhost:3000/mouse")
+puts "Razer Atheris"
+puts mouse.parse
+puts ""
+keyboard = HTTP.get("http://localhost:3000/keyboard")
+puts "Razer Blackwidow Lite TKL"
+puts keyboard.parse
+puts ""
+mousepad = HTTP.get("http://localhost:3000/mousepad")
+puts "Razer Goliathus Stealth"
+puts mousepad.parse
