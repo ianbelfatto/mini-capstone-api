e0d40021acf07459a5751cbb6a095932e4eee23f
ianbelfatto <ibelfatto@gmail.com>
2021-05-10 15:13:16 -0400

    adds restful create action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,17 @@ class ProductsController < ApplicationController
     render json: products.as_json
   end
 
+  def create
+    product = Product.new(
+      name: params[:name],
+      price: params[:price].to_i,
+      image_url: params[:image_url],
+      description: params[:description]
+    )
+    product.save
+    render json: product.as_json
+  end
+
   def show
     product = Product.find_by(id: params[:id])
     render json: product.as_json
