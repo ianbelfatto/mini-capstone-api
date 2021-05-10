e24233977b0cbfbec19cee0f6b0d7e29441c4a38
ianbelfatto <ibelfatto@gmail.com>
2021-05-10 16:52:20 -0400

    adds destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   post "/products" => "products#create"
   get "/products/:id" => "products#show"
   patch "/products/:id" => "products#update"
-  delete "products/:id" => "products#destroy"
+  delete "/products/:id" => "products#destroy"
 end
