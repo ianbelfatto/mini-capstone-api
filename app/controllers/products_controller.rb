03b436e0c76c71833183ae0825c10586bd8f89a4
ianbelfatto <ibelfatto@gmail.com>
2021-05-14 15:28:18 -0400

    adds sql conditions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,26 @@
 class ProductsController < ApplicationController
   def index
     products = Product.all
+    # SQL CONDITIONS
+    if params[:search]
+      products = products.where("name iLIKE ?", "%#{params[:search]}%")
+    end
+
+    if params[:sort] == "price"
+      products = products.order(:price)
+    end
+
+    if params[:sort] == "price" && params[:sort_order] == "desc"
+      products = products.order(price: :desc)
+    end
+
+    if params[:sort] ==
+      products = products.order(:id)
+    end
+
+    if params[:discount] == "true"
+      products = products.where("price < 100")
+    end
     render json: products
   end
 
@@ -9,10 +29,16 @@ class ProductsController < ApplicationController
       name: params[:name],
       price: params[:price],
       image_url: params[:image_url],
-      description: params[:description]
+      description: params[:description],
+      inventory: params[:inventory]
     )
-    product.save
-    render json: product
+    if product.save
+      # HAPPY PATH
+      render json: product
+    else
+      # SAD PATH
+      render json: {errors: product.errors.full_messages}, status: 422
+    end
   end
 
   def show
@@ -26,8 +52,14 @@ class ProductsController < ApplicationController
     product.price = params[:price] || product.price
     product.image_url = params[:image_url] || product.image_url
     product.description = params[:description] || product.description
-    product.save
-    render json: product
+    product.inventory = params[:inventory] || product.inventory
+    if product.save
+      # HAPPY PATH
+      render json: product
+    else
+      # SAD PATH
+      render json: {errors: product.errors.full_messages}, status: 422
+    end
   end
 
   def destroy
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,17 @@
 class Product < ApplicationRecord
+  # VALIDATIONS --------------------------------------------
+
+  validates :name, presence: true, uniqueness: true
+  validates :price, numericality: {greater_than: 0 }
+  validates :description, length: { maximum: 500 }
+  validates :inventory, numericality: { greater_than_or_equal_to: 0 }
+  validates_format_of :image_url, :with => %r{\.(png|jpg|jpeg|gif)$}i, :message => "must have a valid file type", :multiline => true
+
+  # VALIDATIONS --------------------------------------------
+
+
   def is_discounted? # '?' denotes boolean by default so you could write inside this method - "price < 100"
-    if price < 100
+    if price <= 100
       return true
     else
       return false
