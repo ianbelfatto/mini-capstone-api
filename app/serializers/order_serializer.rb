c2480069f662a328bfae6f9ba75880067fd53be4
ianbelfatto <ibelfatto@gmail.com>
2021-05-24 13:30:36 -0400

    adds user_id to order serializer

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,4 +1,4 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :product, :quantity, :subtotal, :tax, :total
+  attributes :id, :user_id, :product, :quantity, :subtotal, :tax, :total
   belongs_to :product
 end
